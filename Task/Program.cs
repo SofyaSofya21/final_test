6c4cf67bcfc787c64cf7e6201ac03d4c965ee1f4
Sofya <sofyaguseva1@gmail.comgit>
2022-12-26 14:13:17 +0100

    Print Atring Array method added

--- Task/Program.cs
+++ Task/Program.cs
@@ -4,8 +4,7 @@
 // лучше обойтись исключительно массивами.
 
 Console.WriteLine("Введите массив строк через пробелы");
-//string input = Console.ReadLine();
-string input = "dfgg rge 34rgefg 23 1 0 ttt";
+string input = Console.ReadLine();
 
 string[] resultArray = new string[0];
 int approvedLength = 3;
@@ -20,9 +19,23 @@ for (int i = 0; i < inputArray.Length; i++)
         resultArray = AddElementInArray(resultArray, inputArray[i]);
     }
 }
-for (int i = 0; i < resultArray.Length; i++)
+
+PrintStringArray(inputArray);
+Console.Write(" -> ");
+PrintStringArray(resultArray);
+Console.WriteLine();
+
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+void PrintStringArray(string[] array)
 {
-Console.Write(resultArray[i]+ ' ');
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1)
+            Console.Write('"' + array[array.Length - 1] + '"' + "]");
+        else
+            Console.Write('"' + array[i] + '"' + ", ");
+    }
 }
 
 string[] StringSplit(string array)
@@ -51,8 +64,8 @@ string[] StringSplit(string array)
 
 string[] AddElementInArray(string[] array, string newElement)
 {
-    string[] tempArray = new string[array.Length+1];
-    tempArray[tempArray.Length-1] = newElement;
+    string[] tempArray = new string[array.Length + 1];
+    tempArray[tempArray.Length - 1] = newElement;
     for (int i = 0; i < array.Length; i++)
     {
         tempArray[i] = array[i];
